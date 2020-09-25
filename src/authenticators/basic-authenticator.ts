f648e4ea88c8cdcb112fa1af23deeb77b68f21f0
Eduardo Sachser <eduardosachser@MacBook-Pro-de-Eduardo.local>
2020-09-25 00:30:21 -0300

    Continuando implementação da Basic Auth.

--- src/authenticators/basic-authenticator.ts
+++ src/authenticators/basic-authenticator.ts
@@ -1,3 +1,4 @@
+import Knex from 'knex';
 import _ from 'lodash';
 import { EsAuthenticator, IEsAuthenticatorConstructor } from "../core/authenticators";
 import { EsAuthenticatorError } from '../core/errors';
@@ -6,14 +7,26 @@ import { logger } from '../util/logger';
 
 export class EsBasicAuthenticator extends EsAuthenticator {
 
-    private _dbId: string
-    private _query: string
+    private _db: Knex;
+    private _findQuery: string;
+    private _roleQuery: string;
+    private _userColumn: string;
+    private _roleColumn: string;
 
     constructor(name: string, id: string, params: any) {
         super(name, id);
 
-        this._dbId = String(_.get(params, 'dbId'));
-        this._query = String(_.get(params, 'query'));
+        this._findQuery = String(_.get(params, 'findQuery'));
+        this._roleQuery = String(_.get(params, 'roleQuery'));
+        this._userColumn = String(_.get(params, 'userColumn'));
+        this._roleColumn = String(_.get(params, 'roleColumn'));
+        
+        const dbId = String(_.get(params, 'dbId'));
+        this._db = getDatabaseConnection(dbId);
+
+        if (this._db === undefined) {
+            throw new EsAuthenticatorError(EsBasicAuthenticator.name, `Database with id ${dbId} not found`);
+        }
     }
 
     async loadAsync() { }
@@ -22,21 +35,33 @@ export class EsBasicAuthenticator extends EsAuthenticator {
         // Procura no db
         const credentials = Buffer.from(tokenStr).toString('base64').split(':');
 
-        const db = getDatabaseConnection(this._dbId);
-        const search = await db.raw(this._query, credentials).then(r => r);
+        const search = await this._db.raw(this._findQuery, credentials).then(r => r);
+        const user = search[0][0];
 
-        if (search === undefined) {
+        if (user === undefined || user === null) {
             throw new EsAuthenticatorError(EsBasicAuthenticator.name, 'Key error', undefined, 'Invalid credentials provided', 401);
         }
 
-        return search;
+        return user;
     }
 
     async validate(params: any) {
         const token = _.get(params, 'token');
-        const scopesNeeded = _.get(params, 'scope');
+        const roleNeeded = _.get(params, 'role');
 
         const res = await this.verify(token);
+
+        // Se os escopos
+        if (roleNeeded !== undefined) {
+            const rolesQuery = await this._db.raw(this._roleQuery, [_.get(res, this._userColumn)]).then(r => r);
+            const roles = rolesQuery[0].map((r: any) => r[this._roleColumn]);
+            if (roles === undefined || roles === null || roles.length === 0 || !roles.includes(roleNeeded)) {
+                throw new EsAuthenticatorError(EsBasicAuthenticator.name, 'Key error', undefined, 'User has no needed role', 401);
+            }
+            res.roles = roles;
+        }
+
+        return res;
     }
 }
 
